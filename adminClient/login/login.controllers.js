013963f4e436c1a140c40a37496bbac7b430fa73
tiantianquan <fenqiufen@gmail.com>
2015-11-30 14:44:32 +0800

    登陆界面

--- adminClient/login/login.controllers.js
+++ adminClient/login/login.controllers.js
@@ -2,37 +2,34 @@ var angular = require('angular')
 var style = require('./login.scss')
 
 module.exports = angular.module('login.controllers', [])
-  .controller('LoginCtrl', function ($scope,$state,$cookies, Login) {
-    $scope.login = function () {
+  .controller('LoginCtrl', function($scope, $state, $cookies, Login) {
+    $scope.login = function() {
       //写cookie
 
-      Login.post($scope.author, function (state) {
-        console.log(state)
+      Login.post($scope.author, function(state) {
         $state.go('/')
-      },function(){
-        console.log(arguments)
-      })
+      }, function() {})
     }
   })
 
-  .controller('RegisterCtrl', function ($scope, Author, $state) {
+.controller('RegisterCtrl', function($scope, Author, $state) {
     $scope.author = new Author()
-    $scope.submit = function () {
-      $scope.author.$save(function (data) {
+    $scope.submit = function() {
+      $scope.author.$save(function(data) {
         $state.go('login', {})
-      }, function (err) {
+      }, function(err) {
         console.log(err)
       })
     }
   })
-  .directive('passwordSame', function () {
+  .directive('passwordSame', function() {
     return {
       restrict: 'A',
       //scope: ture,
       require: 'ngModel',
-      link: function (scope, element, attributes, ngModel) {
-        ngModel.$validators.passwordSame = function (modelValue) {
-          if (scope.author != undefined)
+      link: function(scope, element, attributes, ngModel) {
+        ngModel.$validators.passwordSame = function(modelValue) {
+          if(scope.author != undefined)
             return modelValue == scope.author.password
         }
       }
