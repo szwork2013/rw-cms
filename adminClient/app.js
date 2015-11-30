953220f5309b37240542ffa8ce61dc8cb0a4384f
tiantianquan <fenqiufen@gmail.com>
2015-11-30 14:22:50 +0800

    Revert "no message"
    This reverts commit b6c25a5d2b59f3ef7362f0bcbc797d53eddbd1fd.

--- adminClient/app.js
+++ adminClient/app.js
@@ -8,10 +8,6 @@ var uiRouter = require('angular-ui-router')
 var ngCookies = require('angular-cookies')
 var ngFileUpload = require('ng-file-upload')
 
-//路由
-var router = require('./router')
-//初始化运行
-var initRun = require('./initRun')
 //controller
 var article = require('./article')
 var category = require('./category')
@@ -39,7 +35,7 @@ require('font-awesome/css/font-awesome.css')
 
 var app = angular.module('admin', ['ngSanitize', 'textAngular', ngFileUpload,
   uiRouter, ngCookies,
-  ngMaterial, ngMessages, ngResource,router.name,initRun.name, article.name, login.name, frame.name,
+  ngMaterial, ngMessages, ngResource, article.name, login.name, frame.name,
   category.name,
   homepage.name, aboutpage.name, layout.name, models.name, selfUploadImage.name
 ])
@@ -50,9 +46,175 @@ var app = angular.module('admin', ['ngSanitize', 'textAngular', ngFileUpload,
 
   //设置主题
   $mdThemingProvider.theme('default')
-  //拦截http请求,进行验证
+
   $httpProvider.interceptors.push('LoginInterceptor')
 
+  $stateProvider
+    .state('home', {
+      url: '',
+      views: {
+        'out-wrapper': {
+          templateUrl: 'frame/templates/frame.html'
+        }
+      }
+    })
+
+  .state('login', {
+      url: '/login',
+      views: {
+        'out-wrapper': {
+          templateUrl: 'login/templates/login.html',
+          controller: 'LoginCtrl'
+        }
+      }
+    })
+    .state('register', {
+      url: '/register',
+      views: {
+        'out-wrapper': {
+          templateUrl: 'login/templates/register.html',
+          controller: 'RegisterCtrl'
+        }
+      }
+    })
+
+  .state('home.category', {
+    url: '/category',
+    views: {
+      'main': {
+        templateUrl: 'category/templates/category.html',
+        controller: 'CategoryCtrl'
+      }
+    }
+  })
+
+  .state('home.category.setting', {
+    url: '/setting'
+  })
+
+
+  .state('home.category.setting.create', {
+    url: '/create',
+    views: {
+      'submain@home.category': {
+        templateUrl: 'category/templates/category.setting.html',
+        controller: 'CategorySettingCreateCtrl'
+      }
+    }
+
+  })
+
+
+  .state('home.category.setting.edit', {
+    url: '/:id',
+    views: {
+      'submain@home.category': {
+        templateUrl: 'category/templates/category.setting.html',
+        controller: 'CategorySettingEditCtrl'
+      }
+    }
+  })
+
+
+  .state('home.category.articles', {
+    url: '/:id',
+    views: {
+      'submain': {
+        templateUrl: 'category/templates/category.articles.html',
+        controller: 'CategoryArticlesCtrl'
+      }
+    }
+  })
+
+
+
+  .state('home.article', {
+    url: '/article',
+  })
+
+  .state('home.article.create', {
+    url: '/create',
+    views: {
+      'main@home': {
+        templateUrl: 'article/templates/article.edit.html',
+        controller: 'ArticleCreateCtrl'
+      }
+    },
+    params: {
+      category: null
+    }
+  })
+
+  .state('home.article.edit', {
+      url: '/:id',
+      views: {
+        'main@home': {
+          templateUrl: 'article/templates/article.edit.html',
+          controller: 'ArticleEditCtrl'
+        }
+      }
+    })
+    .state('home.layout', {
+      url: '/layout',
+      views: {
+        'main': {
+          templateUrl: 'layout/templates/layout.html',
+          controller: 'LayoutCtrl'
+        }
+      }
+    })
+
+  .state('home.aboutpage', {
+    url: '/aboutpage',
+    views: {
+      'main': {
+        templateUrl: 'aboutpage/templates/aboutpage.html',
+        controller: 'AboutpageCtrl'
+      }
+    }
+  })
+
+  .state('home.homepage', {
+    url: '/homepage',
+    views: {
+      'main': {
+        templateUrl: 'homepage/templates/homepage.html',
+        controller: 'HomepageCtrl'
+      }
+    }
+  })
+
+  $urlRouterProvider.otherwise('/')
+})
+
+.run(function($rootScope, $cookies, $state, Login) {
+  $rootScope.isLogin = false
+  Login.authUser(undefined,undefined,function(res){
+    if(res.status !== 401) {
+      $rootScope.isLogin = true
+    }
+  })
+
+  //登陆重定向
+  $rootScope.$on('$stateChangeStart',
+    function(event, toState, toParams, fromState, fromParams) {
+      if($rootScope.isLogin || toState.name === 'login') {
+        $rootScope.isLogin = false
+      } else {
+        //先验证再进行页面跳转
+        event.preventDefault()
+        Login.authUser(toState, toParams, function(res) {
+          if(res.status !== 401) {
+            $rootScope.isLogin = true
+            $state.go(toState.name, toParams)
+          }
+        })
+      }
+    })
+
+  $rootScope.toUrl = function(url, params, options) {
+    $state.go(url, params, options)
+  }
 })
 
 .controller('ctrl', function($scope, $http, $state) {
