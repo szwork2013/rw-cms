e2d20b1d874aa10562d01885885c69945f92bfa9
tiantianquan <fenqiufen@gmail.com>
2015-12-08 10:05:57 +0800

    ie8 兼容

--- commonClient/app.js
+++ commonClient/app.js
@@ -1,7 +1,14 @@
 require('./sass/style.scss')
 
-
 var $ = require('jquery')
+
+if(!document.addEventListener) {
+  /**
+   * ie8 fallback
+   */
+  // $('.area-title').css('top', '-40px')
+}
+
 
 $(document).ready(function() {
   var headerNav = $('header nav')
