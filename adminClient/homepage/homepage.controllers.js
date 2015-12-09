a8c2b5b4ab3dc19c4dced03075b60582c01f840a
tiantianquan <fenqiufen@gmail.com>
2015-12-09 12:46:35 +0800

    no message

--- adminClient/homepage/homepage.controllers.js
+++ adminClient/homepage/homepage.controllers.js
@@ -4,63 +4,60 @@ var _ = require('lodash')
 
 module.exports = angular.module('homepage.controllers', [])
   .controller('HomepageCtrl', function($scope, $log, Homepage, Category) {
+    $scope.init = function() {
+      $scope.homepage.area1 = $scope.homepage.area1 || {}
+      $scope.homepage.area2 = $scope.homepage.area2 || {}
+      $scope.homepage.area3 = $scope.homepage.area3 || {}
+
+      $scope.homepage.area1.items = []
+      $scope.homepage.area2.items2 = []
+      $scope.homepage.area3.items3 = []
+
+      $scope.selectedItem1 = _.find($scope.categorys, function(category) {
+        return category._id == $scope.homepage.area1.category
+      })
+      $scope.selectedItem2 = _.find($scope.categorys, function(category) {
+        return category._id == $scope.homepage.area2.category
+      })
+      $scope.selectedItem3 = _.find($scope.categorys, function(category) {
+        return category._id == $scope.homepage.area3.category
+      })
+    }
+
+    //构造函数
+    function searchTextChangeFn(area) {
+      return function(searchText) {
+        var result = searchText ? $scope.categorys.filter(function(
+          category) {
+          return category.name.indexOf(searchText) === 0
+        }) : $scope.categorys
+        area.items = result.length === 0 ? $scope.categorys : result
+      }
+    }
+
+    function selectedItemChangeFn(area) {
+      return function(item) {
+        area.category = item === undefined ? undefined :
+          item._id
+      }
+    }
+
     Category.query(function(categorys) {
       $scope.categorys = _.toArray(categorys)
 
       Homepage.get(function(homepage) {
         $scope.homepage = homepage
-        $scope.homepage.area1 = $scope.homepage.area1 || {}
-        $scope.homepage.area2 = $scope.homepage.area2 || {}
-        $scope.homepage.area3 = $scope.homepage.area3 || {}
+        $scope.init()
 
         //--------------
-        $scope.selectedItem1 = _.find($scope.categorys, function(category) {
-          return category._id == $scope.homepage.area1.category
-        })
-        $scope.selectedItem2 = _.find($scope.categorys, function(category) {
-          return category._id == $scope.homepage.area2.category
-        })
-        $scope.selectedItem3 = _.find($scope.categorys, function(category) {
-          return category._id == $scope.homepage.area3.category
-        })
-        $scope.searchTextChange1 = function(searchText) {
-          var result = searchText ? $scope.categorys.filter(function(category) {
-            return category.name.indexOf(searchText) === 0
-          }) : $scope.categorys
-          $scope.items1 = result.length === 0 ? $scope.categorys : result
-        }
+        $scope.searchTextChange1 = searchTextChangeFn($scope.homepage.area1)
+        $scope.selectedItemChange1 = selectedItemChangeFn($scope.homepage.area1)
+        $scope.searchTextChange2 = searchTextChangeFn($scope.homepage.area2)
+        $scope.selectedItemChange2 = selectedItemChangeFn($scope.homepage.area2)
+        $scope.searchTextChange3 = searchTextChangeFn($scope.homepage.area3)
+        $scope.selectedItemChange3 = selectedItemChangeFn($scope.homepage.area3)
 
-        $scope.selectedItemChange1 = function(item) {
-          $scope.homepage.area1.category = item === undefined ? undefined :
-            item._id
-        }
-
-        $scope.searchTextChange2 = function(searchText) {
-          var result = searchText ? $scope.categorys.filter(function(category) {
-            return category.name.indexOf(searchText) === 0
-          }) : $scope.categorys
-          $scope.items2 = result.length === 0 ? $scope.categorys : result
-        }
-
-        $scope.selectedItemChange2 = function(item) {
-          $scope.homepage.area2.category = item === undefined ? undefined :
-            item._id
-        }
-
-        $scope.searchTextChange3 = function(searchText) {
-          var result = searchText ? $scope.categorys.filter(function(category) {
-            return category.name.indexOf(searchText) === 0
-          }) : $scope.categorys
-          $scope.items3 = result.length === 0 ? $scope.categorys : result
-        }
-
-        $scope.selectedItemChange3 = function(item) {
-            $scope.homepage.area3.category = item === undefined ? undefined :
-              item._id
-          }
-          //----------------
-
-
+        //----------------
         $scope.update = function() {
           Homepage.update({
             id: $scope.homepage._id
